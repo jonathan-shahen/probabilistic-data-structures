bfb76d334a700e2f471811d28e298e8fc17391b4
Jonathan Shahen <jonathan.shahen@gmail.com>
2022-09-01 22:52:24 -0700

    count min sketch for redis

--- src/count-min-sketch/size/src/count-min-sketch-size.py
+++ src/count-min-sketch/size/src/count-min-sketch-size.py
@@ -3,14 +3,20 @@ import math
 
 import redis
 
-required_sizes = [
+byteUnits=['B','KiB','MiB','GiB','TiB','PiB']
+def bytesHuman(size,precision=2):
+    unit = int(math.log(size) / math.log(1024))
+    value = size * (1/1024)**(unit)
+    return f'{value: >5.{precision}f}{byteUnits[unit]}'
+
+num_counters = [
     1, 10, 100, 1_000, 10_000, 100_000,
     1_000_000, 10_000_000, 100_000_000,
     1_000_000_000
 ]
 
-error_rates = [
-    1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9
+num_hashes = [
+    1,2,3,5,10,15
 ]
 
 r = redis.Redis(host=os.environ.get('REDIS_HOST'), port=os.environ.get('REDIS_PORT'), db=0)
@@ -19,21 +25,20 @@ r = redis.Redis(host=os.environ.get('REDIS_HOST'), port=os.environ.get('REDIS_PO
 # The number of hash functions is -log(error)/ln(2)^2.
 # The number of bits per item is -log(error)/ln(2) ≈ 1.44.
 
-for error_rate in error_rates:
-    print(f"### Error Rate: {error_rate}")
-    print("| Number of Items | Count Min Sketch Size (bytes) | Memory Usage (bytes) | Error Rate |")
-    print("|---|---|---|---|---|")
-    for size in required_sizes:
-        count_min_sketch = f"count_min_sketch_{size}_{error_rate}"
+for num_hash in num_hashes:
+    print(f"### Number Hashes: {num_hash}")
+    print("| Number of Hashes | Number of Counters | Memory Usage (human)| Memory Usage (bytes) |")
+    print("|---|---|---|---|")
+    for num_counter in num_counters:
+        count_min_sketch = f"count_min_sketch_{num_counter}_{num_hash}"
         # print(f"Creating Count Min Sketch {count_min_sketch}")
         r.delete(count_min_sketch)
-        filter = r.bf().create(count_min_sketch, error_rate, size, noScale=True)
-        info = r.bf().info(count_min_sketch)
+        filter = r.cms().initbydim(count_min_sketch, num_counter, num_hash)
 
         # The MEMORY USAGE command reports the number of bytes that a key and its value require to be stored in RAM.
         mem_usage = r.memory_usage(count_min_sketch)
 
-        print(f"| {size:,} | {info.size:,} | {mem_usage:,} | {error_rate} |")
+        print(f"| {num_hash} | {num_counter:,} | {bytesHuman(mem_usage)} | {mem_usage:,} |")
     print("")
 print("")
 print("You can connect to Redis and inspect the newly created Count Min Sketch.")
