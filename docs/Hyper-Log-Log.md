25126add67ea62991c438185b795feeaf4e18962
Jonathan Shahen <jonathan.shahen@gmail.com>
2022-09-05 16:41:28 -0700

    HLL size and results

--- docs/Hyper-Log-Log.md
+++ docs/Hyper-Log-Log.md
@@ -2,6 +2,24 @@
 
 ## Overview
 
+Hyper Log Log (HLL) is a probabilistic algorithm/data-structure for estimating the number of unique elements in a set
+(cardinality). HLL's utilize statistics to come up with their estimates and assumes that the values being inserted
+are from a random distribution, this means that the bits of an element have the same probability of being selected.
+For normal data streams, utilizing the raw item will likely break this assumption, thus HLL's utilize hashing to come up
+with a random value (hash functions are not random functions, but they can appear random).
+
+HLLs work for situations where storing a linear list is impractical.
+HLLs store the largest number of trailing zeros for a number, to increase the accuracy of the measurement HLLs have
+multiple buckets, where each bucket stores the maximum number of trailing zeros seen within that bucket, and the leading
+bits are used to find which bucket a hash should be compared to.
+Estimates are made by taking the harmonic average of the number of trailing bits from each bucket; depending on the
+harmonic average, there are multiple methods to to convert this to an estimate (these method are shown to empirically
+improve the estimate).
+
+
+**NOTE**: HLL estimates get more accurate with more inserts. HLLs have corrections for small, medium, large ranges.
+But for space efficiency, it might be better to store the raw inserts linearly until the storage requirements increase
+to the range of an HLL.
 
 
 ## Quick Features
--- src/hyper-log-log/size/src/hyper-log-log-size.py
+++ src/hyper-log-log/size/src/hyper-log-log-size.py
@@ -9,37 +9,24 @@ def bytesHuman(size,precision=2):
     value = size * (1/1024)**(unit)
     return f'{value: >5.{precision}f}{byteUnits[unit]}'
 
-num_counters = [
-    1, 10, 100, 1_000, 10_000, 100_000,
-    1_000_000, 10_000_000, 100_000_000,
-    1_000_000_000
-]
-
-num_hashes = [
-    1,2,3,5,10,15
-]
-
 r = redis.Redis(host=os.environ.get('REDIS_HOST'), port=os.environ.get('REDIS_PORT'), db=0)
 
-# From redis docs: https://redis.io/commands/bf.reserve/
-# The number of hash functions is -log(error)/ln(2)^2.
-# The number of bits per item is -log(error)/ln(2) ≈ 1.44.
+print("### HLL Size per Elements Added")
+print("| Number of Items | HLL Estimate | Percent Error (%) | HLL Size (human) | HLL Size (bytes) |")
+print("|---|---|---|---|---|")
+for num_items in [1e1,1e2,1e3,1e4,1e5,1e6]:
+    hll = f"hll_{num_items}_items"
+    r.delete(hll)
 
-for num_hash in num_hashes:
-    print(f"### Number Hashes: {num_hash}")
-    print("| Number of Hashes | Number of Counters | Memory Usage (human)| Memory Usage (bytes) |")
-    print("|---|---|---|---|")
-    for num_counter in num_counters:
-        count_min_sketch = f"count_min_sketch_{num_counter}_{num_hash}"
-        # print(f"Creating Count Min Sketch {count_min_sketch}")
-        r.delete(count_min_sketch)
-        filter = r.cms().initbydim(count_min_sketch, num_counter, num_hash)
+    for i in range(int(num_items/10)):
+        r.pfadd(hll, 10*i, 10*i + 1, 10*i + 2, 10*i + 3, 10*i + 4, 10*i + 5, 10*i + 6, 10*i + 7, 10*i + 8, 10*i + 9)
 
-        # The MEMORY USAGE command reports the number of bytes that a key and its value require to be stored in RAM.
-        mem_usage = r.memory_usage(count_min_sketch)
+    estimate = r.pfcount(hll)
+    percent_error = abs(num_items - estimate) / num_items
+    # The MEMORY USAGE command reports the number of bytes that a key and its value require to be stored in RAM.
+    mem_usage = r.memory_usage(hll)
 
-        print(f"| {num_hash} | {num_counter:,} | {bytesHuman(mem_usage)} | {mem_usage:,} |")
-    print("")
-print("")
-print("You can connect to Redis and inspect the newly created Count Min Sketch.")
+    print(f"| {num_items:.1E} | {estimate:.1E} | {percent_error: >6,.1%} | {bytesHuman(mem_usage): >9} | {mem_usage:,} |")
+
+print("You can connect to Redis and inspect the newly created HLL.")
 print(f"Redis Instance: localhost:{os.environ.get('REDIS_PORT')}")
